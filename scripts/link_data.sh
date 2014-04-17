a23fd6a99217efe5d5e4c175b8bf40926b4dafea
Matt Post <post@cs.jhu.edu>
2014-04-17 19:48:12 -0400

    Fixed bugs reported by Jay DeYoung

--- scripts/link_data.sh
+++ scripts/link_data.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+mkdir data
+
 for prefix in train dtest etest; do
   for suffix in form tag lemma tree; do
-    ln -s ~mpost/data/en600.468/inflect /data/$prefix.$suffix data/$prefix.$suffix
+    ln -s ~mpost/data/en600.468/inflect/data/$prefix.$suffix data/$prefix.$suffix
   done
 done
