7c7f5129dd2fa8c3f2e86344b4480d7f5c0d8518
Samantha Siow <ssiow1@jhu.edu>
2016-04-14 12:36:37 -0400

    completed bigram model, still need to implement comparison between unigrams + bigrams

--- scripts/bigram_inflect.py
+++ scripts/bigram_inflect.py
@@ -25,27 +25,45 @@ args = PARSER.parse_args()
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 sys.stdin = codecs.getreader('utf-8')(sys.stdin)
 
-def inflections(lemma):
-    if LEMMAS.has_key(lemma):
-        return sorted(LEMMAS[lemma].keys(), lambda x,y: cmp(LEMMAS[lemma][y], LEMMAS[lemma][x]))
+def inflections(lemma, lemmas):
+    if lemmas.has_key(lemma):
+        return sorted(lemmas[lemma].keys(), lambda x,y: cmp(lemmas[lemma][y], lemmas[lemma][x]))
     return [lemma]
 
-def best_inflection(lemma):
-    return inflections(lemma)[0]
+def best_inflection(lemma, lemmas):
+    return inflections(lemma, lemmas)[0]
 
 if __name__ == '__main__':
 
     # Build a simple unigram model on the training data
-    LEMMAS = defaultdict(defaultdict)
+    UNIGRAM_LEMMAS = defaultdict(defaultdict)
+    BIGRAM_LEMMAS = defaultdict(defaultdict)
     if args.t:
+        # combine creates the test file names = e.g. train.lemma, and train.form
         def combine(a, b): return '%s.%s' % (a, b)
         def utf8read(file): return codecs.open(file, 'r', 'utf-8')
-        # Build the LEMMAS hash, a two-level dictionary mapping lemmas to inflections to counts
+        # Build the UNIGRAM_LEMMAS hash, a two-level dictionary mapping lemmas to inflections to counts
+        # combine 'words' from train.form, and 'lemmas' from train.lemma
         for words, lemmas in izip(utf8read(combine(args.t, args.w)), utf8read(combine(args.t, args.l))):
-            for word, lemma in izip(words.rstrip().lower().split(), lemmas.rstrip().lower().split()):
-                print 'lemma', lemma,'word',  word
-		        LEMMAS[lemma][word] = LEMMAS[lemma].get(word,0) + 1
+            print 'words from train.form: ', words
+            print 'lemmas from train.lemma: ', lemmas
+            lemmas_array = lemmas.rstrip().lower().split()
+            words_array = words.rstrip().lower().split()
+            for i,word in enumerate(words_array):
+                # might have an out of bounds error if len(lemmas) < len(words)
+                lemma = lemmas_array[i]
+                print '\t', 'word: ', word, ', lemma:', lemma
+                UNIGRAM_LEMMAS[lemma][word] = UNIGRAM_LEMMAS[lemma].get(word,0) + 1
+                if i < len(words_array)-1:
+                    lemma_bigram = (lemma, lemmas_array[i+1])
+                    inflection_bigram = (word, words_array[i+1])
+                    BIGRAM_LEMMAS[lemma_bigram][inflection_bigram] = BIGRAM_LEMMAS[lemma_bigram].get(inflection_bigram,0) + 1
+
+
+
+        for lemma in BIGRAM_LEMMAS:
+            print 'Lemma:' , lemma, ', inflections: ', BIGRAM_LEMMAS[lemma]
 
     # Choose the most common inflection for each word and output them as a sentence
     for line in sys.stdin:
-         ' '.join([best_inflection(x) for x in line.rstrip().split()])
+         ' '.join([best_inflection(x, UNIGRAM_LEMMAS) for x in line.rstrip().split()])
