12ae29d8c38c3b418c7beb9d7aa999fc29a217ef
carlapcastro <carlacastro47@gmail.com>
2016-04-19 15:28:27 -0400

    adding trigram, output files and change to bigram

--- scripts/tri.py
+++ scripts/tri.py
@@ -25,22 +25,29 @@ args = PARSER.parse_args()
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 sys.stdin = codecs.getreader('utf-8')(sys.stdin)
 
-def inflections(lemma, lemmas):
-    if lemmas.has_key(lemma):
+# combine creates the test file names = e.g. train.lemma, and train.form
+def combine(a, b): return '%s.%s' % (a, b)
+def combine3(a,b,c): return '%s.%s.%s' %(a,b,c)
+
+def inflections(before_prev_lemma, prev_lemma, lemma, lemmas):
+    if lemmas.has_key(combine3(before_prev_lemma,prev_lemma,lemma)):
+        return sorted(lemmas[combine3(before_prev_lemma,prev_lemma,lemma)].keys(),
+                      lambda x,y: cmp(lemmas[combine3(before_prev_lemma,prev_lemma,lemma)][y], lemmas[combine3(before_prev_lemma,prev_lemma,lemma)][x]))
+    elif lemmas.has_key(combine(prev_lemma,lemma)):
+        return sorted(lemmas[combine(prev_lemma,lemma)].keys(), lambda x,y: cmp(lemmas[combine(prev_lemma,lemma)][y], lemmas[combine(prev_lemma,lemma)][x]))
+    elif lemmas.has_key(lemma):
         return sorted(lemmas[lemma].keys(), lambda x,y: cmp(lemmas[lemma][y], lemmas[lemma][x]))
     return [lemma]
 
-def best_inflection(lemma, lemmas):
-    return inflections(lemma, lemmas)[0]
+def best_inflection(before_prev_lemma, prev_lemma, lemma, lemmas):
+    return inflections(before_prev_lemma, prev_lemma, lemma, lemmas)[0]
 
 if __name__ == '__main__':
 
     # Build a simple unigram model on the training data
-    UNIGRAM_LEMMAS = defaultdict(defaultdict)
-    BIGRAM_LEMMAS = defaultdict(defaultdict)
+    LEMMAS = defaultdict(defaultdict)
     if args.t:
-        # combine creates the test file names = e.g. train.lemma, and train.form
-        def combine(a, b): return '%s.%s' % (a, b)
+
         def utf8read(file): return codecs.open(file, 'r', 'utf-8')
         # Build the UNIGRAM_LEMMAS hash, a two-level dictionary mapping lemmas to inflections to counts
         # combine 'words' from train.form, and 'lemmas' from train.lemma
@@ -49,41 +56,27 @@ if __name__ == '__main__':
             # print 'lemmas from train.lemma: ', lemmas
             lemmas_array = lemmas.rstrip().lower().split()
             words_array = words.rstrip().lower().split()
-            for i,word in enumerate(words_array):
-                # might have an out of bounds error if len(lemmas) < len(words)
-                lemma = lemmas_array[i]
-                # print '\t', 'word: ', word, ', lemma:', lemma
-                UNIGRAM_LEMMAS[lemma][word] = UNIGRAM_LEMMAS[lemma].get(word,0) + 1
-                if i < len(words_array)-1:
-                    lemma_bigram = (lemma, lemmas_array[i+1])
-                    inflection_bigram = (word, words_array[i+1])
-                    BIGRAM_LEMMAS[lemma_bigram][inflection_bigram] = BIGRAM_LEMMAS[lemma_bigram].get(inflection_bigram,0) + 1
+            before_last = "<s>"
+            last = "<s>"
+            for word, lemma in izip(words_array, lemmas_array):
+                LEMMAS[lemma][word] = LEMMAS[lemma].get(word, 0) + 1
+                LEMMAS[combine(last, lemma)][word] = LEMMAS[combine(last, lemma)].get(word, 0) + 1
+                LEMMAS[combine3(before_last, last, lemma)][word] = LEMMAS[combine3(before_last, last, lemma)].get(word, 0) + 1
+                before_last = last
+                last = lemma
 
-        # for lemma in BIGRAM_LEMMAS:
-            # print 'Lemma:' , lemma, ', inflections: ', BIGRAM_LEMMAS[lemma]
 
     # Choose the most common inflection for each word and output them as a sentence
     for line in sys.stdin:
-        # check i,i+1 for bigram:
-        # if doesn't exist:
-            # check i for unigram:
-            # i += 1
-        # skip i+1, move to i=i+2
-
         idx = 0
         lemmas = line.rstrip().lower().split()
         inflection_hyp = []
+        last = "<s>"
+        before_last = "<s>"
         while idx < len(lemmas):
-            if idx < len(lemmas)-1:
-                bigram = (lemmas[idx],lemmas[idx+1])
-                if bigram in BIGRAM_LEMMAS:
-                    best_bigram_inflection = best_inflection(bigram, BIGRAM_LEMMAS)
-                    inflection_hyp.append(best_bigram_inflection[0])
-                    inflection_hyp.append(best_bigram_inflection[1])
-                    idx += 2
-                    continue
-            best_unigram_inflection = best_inflection(lemmas[idx], UNIGRAM_LEMMAS)
-            inflection_hyp.append(best_unigram_inflection)
+            inflection_hyp.append(best_inflection(before_last, last, lemmas[idx], LEMMAS))
+            before_last = last
+            last = lemmas[idx]
             idx += 1
-
         print ' '.join(inflection_hyp)
+
